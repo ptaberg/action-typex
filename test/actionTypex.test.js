87d048bc315e126f7396ffad08b9865c2adf05e2
Kolja Ptaberg <34740585+ptaberg@users.noreply.github.com>
2020-02-25 10:14:08 +0300

    Update actionTypex.test.js

--- test/actionTypex.test.js
+++ test/actionTypex.test.js
@@ -16,21 +16,6 @@ describe('configureActionTypes', () => {
     });
 });
 
-describe('createActionTypesGroup', () => {
-    it('should return the object with action types', () => {
-        expect(typeof resultOfGroup).toBe('object');
-    });
-    it('should return action types number equals to initial config', () => {
-        expect(Object.keys(resultOfGroup).length).toBe(3);
-    });
-});
-
-describe('createStaticActions', () => {
-    it('should return an object with number of keys equals number of arguments', () => {
-      expect(Object.keys(resultOfStatic).length).toBe(2);
-    });
-});
-
 describe('combineActionTypes', () => {
     it('should return an object', () => {
       expect(typeof resultOfCombined).toBe('object');
